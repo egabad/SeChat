c23bf129087bbc47a4ae23b1a3256d88fea89862
Earl Andrea Abad <earl1a@ai-pros.com>
2021-06-09 09:36:40 +0800

    Added basic room functionality

--- index.js
+++ index.js
@@ -2,22 +2,36 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
-const { Server } = require("socket.io");
+const { Server } = require('socket.io');
 const io = new Server(server);
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
+let room = '';
 io.on('connection', (socket) => {
-    console.log('a user connected');
-    socket.on('chat message', (msg) => {
-        console.log('message: ' + msg);
-        // Broadcast message to everyone
-        io.emit('chat message', msg);
+    console.log(`user "${socket.id}" connected`);
+    socket.on('join room', (data) => {
+        console.log(`in room "${data.room}"`);
+        
+        room = data.room;
+        socket.join(room);
+
+        let msg = `user ${socket.id} has joined room ${room}`;
+        io.to(room).emit('chat message', { msg: msg, id: socket.id });
     });
+
+    socket.on('chat message', (data) => {
+        console.log(`message in ${data.room}: ${data.msg}`);
+        // Broadcast message to everyone
+        io.to(data.room).emit('chat message', { msg: data.msg, id: socket.id });
+    });
+
     socket.on('disconnect', () => {
         console.log('user disconnected');
+        let msg = `user ${socket.id} has left room ${room}`
+        io.to(room).emit('chat message', { msg: msg, id: socket.id });
     });
 });
 
